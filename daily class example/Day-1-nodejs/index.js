7cae968926b9a872415d15da3fbc748d2b9c33e8
bavi2477 <bavithra.sjh@gmail.com>
2024-03-06 12:51:35 +0530

    first commit

---
+++ daily class example/Day-1-nodejs/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+// app.use() //middleware
+
+//getcall 
+app.get('/', (req,res) => {
+    res.status(200).json({message: "Hai my dear mentor, we will work to become a FSD developer"})
+}) 
+
+app.get('/get-data', (req,res) => {
+    res.status(200).json({message: "data", data:{name:"Bavithra"}})
+}) 
+
+//running 
+app.listen(PORT, () => {
+    console.log(`App i running in the port  ${PORT}`);
+}) 
